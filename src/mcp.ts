a067b48aa14d2543b0e8e27e10587771fb7afd25
Pavel Jbanov <pavelgj@gmail.com>
2025-07-14 07:59:59 -0400

    fix: more fixes to the bin script

--- src/mcp.ts
+++ src/mcp.ts
@@ -1,12 +1,11 @@
-import { googleAI } from '@genkit-ai/googleai';
+#!/usr/bin/env node
+
 import { createMcpServer } from '@genkit-ai/mcp';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { genkit } from 'genkit/beta';
 import { defineMemoryTools } from './kv_tools.js';
 
-const ai = genkit({
-  plugins: [googleAI()],
-});
+const ai = genkit({});
 
 defineMemoryTools(ai);
 
